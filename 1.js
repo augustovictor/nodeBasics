d75bf26b318c29192667f6d9c57bb8d498fa9454
Victor <victoraweb@gmail.com>
2016-03-16 07:15:04 -0300

    Validation of attribute

--- 1.js
+++ 1.js
@@ -1,6 +1,18 @@
-#!/usr/bin/env node
+function printHello() {
+    console.log("1.js (c) Victor Augusto");
+    console.log("");
+    console.log("Usage:");
+    console.log("--help            print this help");
+    console.log("--name            say hello {name}");
+    console.log("");
+}
 
 var args = require('minimist')(process.argv.slice(2), { string: "name" }); // Do not use single quotes
+
+if(args.help || !args.name) {
+    printHello();
+    process.exit(1);
+}
 
 var name = args.name;
 
