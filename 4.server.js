c2e37606ba34be5cf09e691387f67dfac2dbe7aa
Victor <victoraweb@gmail.com>
2016-03-17 10:17:27 -0300

    Creating a server and defining a route

---
+++ 4.server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var port = 3000;
+var host = 'localhost';
+
+function httpHandler(req, res) {
+    if(req.method === 'GET') {
+        if(req.url === '/') {
+            res.writeHead(200, {'Content-type': 'text/plain'});
+            res.end("Hellow World! " + Math.random());
+        }
+        else {
+            sayGetOut();
+        }
+    }
+    else {
+        sayGetOut();
+    }
+}
+
+function sayGetOut() {
+    res.writeHead(403);
+    res.end('Get outta here!');
+}
+
+var server = http.createServer(httpHandler)
+    .listen(port, host);
+
+server.listen(port, function() {
+    console.log('Server running at http://' + host + ':' + port);
+});
